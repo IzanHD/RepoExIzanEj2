a9e75011d180c80acb3f87d2f9085495535b0bce
IzanHD <izang558@gmail.com>
2024-05-28 10:14:24 +0200

    Creación constructor y metodos de clase AnalisisEdad

--- ExamenIzanEj2/src/AnalisisEdad.java
+++ ExamenIzanEj2/src/AnalisisEdad.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
 
 public class AnalisisEdad {
+	private ArrayList<Integer> edades;
 
+
+	/**
+	 * @param edades
+	 */
+	public AnalisisEdad(ArrayList<Integer> edades) {
+		super();
+		this.edades = edades;
+	}
+	
+	
+	public String contarEdades() {
+		String cadena="";
+		int contMen = 0, contMay = 0, contJub = 0;
+		
+		for(int i = 0; i<edades.size(); i++) {
+			if (edades.get(i) < 18) {
+				contMen++;
+			}else if(edades.get(i) < 65) {
+				contMay++;
+			}else {
+				contJub++;
+			}
+		}
+		
+		cadena = "Menores de edad: " + contMen + ", Mayores de edad: " + contMay + ", Jubilidados: " + contJub;
+		
+		return cadena;
+		
+	}
 }
