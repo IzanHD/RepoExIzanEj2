5b612f7d8bb3c0c7d9e4eb88525215905def99b1
IzanHD <izang558@gmail.com>
2024-05-28 10:26:44 +0200

    Creación de funcionalidad PrincipalApp

--- ExamenIzanEj2/src/PrincipalApp.java
+++ ExamenIzanEj2/src/PrincipalApp.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
 
 public class PrincipalApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		ArrayList<Integer> edades = new ArrayList<>();
+		
+		edades.add(20);
+		edades.add(30);
+		edades.add(2);
+		edades.add(70);
+		edades.add(90);
+		edades.add(40);
+		edades.add(24);
+		
+		AnalisisEdad edadesObj = new AnalisisEdad(edades);
+		
+		System.out.println(edadesObj.contarEdades());
+		
 	}
 
 }
